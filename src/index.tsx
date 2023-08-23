641fd6c041eddcd45d5ea2e95a832a0839b669c2
KimEijin <jianchidaodi1029@naver.com>
2023-08-23 12:34:34 +0900

    Chore.eslint설정중

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import store from './redux/config/confingStore';
 import { Provider } from 'react-redux';
@@ -13,7 +12,7 @@ root.render(
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>
+  </Provider>,
 );
 
 reportWebVitals();
