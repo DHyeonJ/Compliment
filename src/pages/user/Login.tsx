932126a8e5cf0d41225a7ee9e1563ad2a4c534ca
KimEijin <jianchidaodi1029@naver.com>
2023-08-25 20:32:12 +0900

    Feat.SignUP(아이디,비밀번호)-파이어베이스와 연동완료

--- src/pages/user/Login.tsx
+++ src/pages/user/Login.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
 function Login() {
-  return <div>Login화면입니다.</div>
+  return (
+    <>
+      <div>
+        <div>
+          <div>Logo</div>
+          <div>칭찬을 구해요, 칭구</div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Login
