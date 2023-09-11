e6eea6cb8294596de79fa5ac2ac2da3dbc566f33
Persimmon <53931787+DHyeonJ@users.noreply.github.com>
2023-09-11 16:15:46 +0900

    fix. reset Css 수정, main UI 일부 수정

--- src/index.jsx
+++ src/index.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import store from './redux/config/confingStore'
 import { Provider } from 'react-redux'
 import { QueryClientProvider, QueryClient } from 'react-query'
+import 'normalize.css' // Reset CSS
 
 const queryClient = new QueryClient()
 const root = ReactDOM.createRoot(document.getElementById('root'))
