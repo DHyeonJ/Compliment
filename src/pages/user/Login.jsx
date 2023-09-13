56d8a6ba6ea887c16b9e8d59268f79cba461176f
KimEijin <jianchidaodi1029@naver.com>
2023-09-13 17:34:20 +0900

    !

--- src/pages/user/Login.jsx
+++ src/pages/user/Login.jsx
@@ -24,6 +24,7 @@ function Login() {
   const [password, setPassword] = useState('')
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [googleUserData, setGoogleUserData] = useState(null)
+  const [isValidEmail, setIsValidEmail] = useState(true)
 
   function handleGoogleLogin() {
     const provider = new GoogleAuthProvider()
